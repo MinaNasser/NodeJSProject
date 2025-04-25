d1d48deb24953f474aa73c70ac5dad3ced2c8fd1
MinaAl5al <minanasser82018@gmail.com>
2025-04-25 23:10:37 +0300

    User Controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,102 +1,92 @@
-
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const cloudinary = require("../config/Cloudinary");
 
-const register = async (req,res) => {
+const register = async (req, res) => {
   if (!req.body) {
     return res.status(400).json({ error: "Missing request body" });
   }
-    const{
-
-      username,
-        email,
-        password,
-        phoneNumber,
-        bio,
-        socialLinks
-    } = req.body
-    const result = await cloudinary.uploader.upload(req.file.path, {
-          resource_type: "image",  // تحديد نوع المحتوى كـ فيديو
-          public_id: `images/${req.file.filename}`,  // تعيين اسم الفيديو في Cloudinary
-          overwrite: true,  // استبدال الفيديو بنفس الاسم إذا كان موجودًا
-          
-        });
-
-try {
-    const existingUser = await User.findOne({email});
-
-    if(existingUser)
-        return res.status(400).json({message : "User already exists"});
-
-    const HashPassword = await bcrypt.hash(password,10);
-
-    const newUser = new User({
-        username,
-        email,
-        password: HashPassword,
-        phoneNumber,
-        avatar: avatar || "", 
-        bio: bio || "",       
-        isEmailVerified: true, 
-        socialLinks: {
-          instagram: socialLinks?.instagram || "",
-          tiktok: socialLinks?.tiktok || "",
-          youtube: socialLinks?.youtube || ""
-      },
-      role: "user",
-      isBlocked: false
+  const { username, email, password, phoneNumber, bio, socialLinks } = req.body;
+  const avatar = await cloudinary.uploader.upload(req.file.path, {
+    resource_type: "image", // تحديد نوع المحتوى كـ فيديو
+    public_id: `images/${req.file.filename}`, // تعيين اسم الفيديو في Cloudinary
+    overwrite: true, // استبدال الفيديو بنفس الاسم إذا كان موجودًا
   });
 
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const HashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: HashPassword,
+      phoneNumber,
+      avatar: avatar?.secure_url || "",
+      bio: bio || "",
+      isEmailVerified: true,
+      socialLinks: {
+        instagram: socialLinks?.instagram || "",
+        tiktok: socialLinks?.tiktok || "",
+        youtube: socialLinks?.youtube || "",
+      },
+      role: "user",
+      isBlocked: false,
+    });
+
     await newUser.save();
-      
-    res.status(201).json({ message: "User registered successfully", user: newUser });
 
-}
-catch(error){
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
+  } catch (error) {
     res.status(500).json({ message: error.message });
+  }
 };
-}
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-const login = async (req,res)=>{
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
 
-    const {email , password} = req.body;
+    const MatchPass = await bcrypt.compare(password, user.password);
+    if (!MatchPass)
+      return res.status(400).json({ message: "Invalid email or password" });
 
-    try{
-
-        const user = await User.findOne({email});
-        if (!user) return res.status(400).json({ message: "Invalid email or password" });
-        
-        const MatchPass = await bcrypt.compare(password,user.password);
-        if (!MatchPass) return res.status(400).json({ message: "Invalid email or password" });
-
-        if (!user.isEmailVerified) {
-            return res.status(403).json({ message: "Please verify your email before logging in." });
-          }
-
-        const token = jwt.sign(
-            { userId: user._id, role: user.role },
-            process.env.JWT_SECRET,
-            { expiresIn: "2d" }
-        )
-
-        res.status(200).json({
-            message: "Login successful",
-            token,
-            user: {
-              id: user._id,
-              username: user.username,
-              email: user.email,
-              avatar: user.avatar,
-              role: user.role
-            }
-          });
+    if (!user.isEmailVerified) {
+      return res
+        .status(403)
+        .json({ message: "Please verify your email before logging in." });
     }
-    catch (err) {
-        res.status(500).json({ message: err.message });
-      }
 
-}
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "2d" }
+    );
 
-module.exports = { register , login  };
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        avatar: user.avatar,
+        role: user.role,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { register, login };
