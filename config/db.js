c0cfda914403169821010428b2084445f32bfa1f
MinaAl5al <minanasser82018@gmail.com>
2025-04-25 17:12:54 +0300

    VideoModel

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
-
+import { config } from 'dotenv';
+config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ Error connecting to MongoDB:", err.message);
